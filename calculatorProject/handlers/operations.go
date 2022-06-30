7199eb16e9e4601566739c8a22b9156d2dd69ac7
Zahraa-Karaki <108420683+Zahraa-Karaki@users.noreply.github.com>
2022-06-30 12:00:40 +0300

    Add files via upload

---
+++ calculatorProject/handlers/operations.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"database/sql"
+	"net/http"
+
+	//"strconv"
+	"github.com/labstack/echo"
+	"github.com/zahraakaraki/MyApp/models"
+)
+
+// AddOperation returns a function that implements the Echo.HandlerFunc interface.
+func AddOperation(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		var operation models.Operation
+
+		c.Bind(&operation)
+
+		answer, err := models.AddOperation(db, operation.FirstNumber, operation.SecondNumber, operation.Operator)
+
+		operation.Answer = answer
+
+		if err == nil {
+			return c.JSON(http.StatusCreated, operation)
+		} else {
+			return c.JSON(http.StatusOK, operation)
+		}
+	}
+}
